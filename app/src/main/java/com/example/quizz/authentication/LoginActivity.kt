c26232bdf847b936672ced792ee0780788b4ae22
JasonHaque <zahinjason220434@gmail.com>
2020-04-14 03:24:34 +0600

    signup activity added

---
+++ app/src/main/java/com/example/quizz/authentication/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.quizz.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quizz.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/example/quizz/authentication/SignUpActivity.kt
+++ app/src/main/java/com/example/quizz/authentication/SignUpActivity.kt
@@ -1,12 +1,13 @@
-package com.example.quizz
+package com.example.quizz.authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quizz.R
 
-class LoginActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up_acitivty)
     }
 }
