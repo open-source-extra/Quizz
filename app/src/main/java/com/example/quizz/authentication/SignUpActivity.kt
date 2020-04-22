901bf1efe0e67895b294dadd8518e246763ebd1a
JasonHaque <zahinjason220434@gmail.com>
2020-04-22 12:58:37 +0600

    sign up method added

--- app/src/main/java/com/example/quizz/authentication/SignUpActivity.kt
+++ app/src/main/java/com/example/quizz/authentication/SignUpActivity.kt
@@ -3,7 +3,9 @@ package com.example.quizz.authentication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.quizz.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_up_acitivty.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -18,6 +20,42 @@ class SignUpActivity : AppCompatActivity() {
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
+
+        signup_button.setOnClickListener {
+            val email = signup_mail.text.toString()
+            val password = signup_password.text.toString()
+            val confirm = confirm_password.text.toString()
+
+            val error = verifyInputs(email, password, confirm)
+            if (error != ""){
+                Toast.makeText(this,error,Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            signUp(email,password)
+        }
+
+    }
+
+    fun verifyInputs(email:String,password:String,confirm:String):String {
+        if (email.isEmpty() || password.isEmpty() || confirm.isEmpty()) {
+            return "Fill up the fields properly"
+        }
+        else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            return "Not a valid email address"
+        }
+        else if (!password.equals(confirm)){
+            return "Passwords do not match"
+        }
+        return ""
+    }
+
+    fun signUp(email:String,password: String){
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+            .addOnSuccessListener {
+                Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
+                return@addOnSuccessListener
+        }
+        return
     }
 
 }
