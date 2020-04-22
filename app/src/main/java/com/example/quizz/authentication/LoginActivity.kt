4f3181a774e78334972e929b4294ea33681fdee4
JasonHaque <zahinjason220434@gmail.com>
2020-04-22 12:14:41 +0600

    added new transition

--- app/src/main/java/com/example/quizz/authentication/LoginActivity.kt
+++ app/src/main/java/com/example/quizz/authentication/LoginActivity.kt
@@ -1,13 +1,22 @@
 package com.example.quizz.authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.quizz.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        bindListeners()
+    }
+    private fun bindListeners(){
+        goto_signup.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/quizz/authentication/SignUpActivity.kt
+++ app/src/main/java/com/example/quizz/authentication/SignUpActivity.kt
@@ -4,13 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.quizz.R
+import kotlinx.android.synthetic.main.activity_sign_up_acitivty.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_acitivty)
-        //transition()
+        bindListeners()
+    }
+    private fun bindListeners(){
+        goto_login.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
