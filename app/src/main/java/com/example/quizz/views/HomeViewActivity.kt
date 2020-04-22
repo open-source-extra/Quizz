974b06c393530d7de38e067c519b96b6b0708abc
JasonHaque <zahinjason220434@gmail.com>
2020-04-22 13:04:01 +0600

    new activity added

---
+++ app/src/main/java/com/example/quizz/views/HomeViewActivity.kt
@@ -0,0 +1,13 @@
+package com.example.quizz.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quizz.R
+
+class HomeViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_view)
+    }
+}
