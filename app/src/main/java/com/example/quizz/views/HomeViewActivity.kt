242497226d91b5bbce1ad19812ae9d924e75cd69
JasonHaque <zahinjason220434@gmail.com>
2020-04-24 12:27:22 +0600

    bound views

--- app/src/main/java/com/example/quizz/views/HomeViewActivity.kt
+++ app/src/main/java/com/example/quizz/views/HomeViewActivity.kt
@@ -3,11 +3,23 @@ package com.example.quizz.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.quizz.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home_view.*
 
 class HomeViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_view)
+        bindViews()
+        bindListeners()
+    }
+    private fun bindViews(){
+        val welcome = user_view.text.toString()
+        user_view.text = welcome + FirebaseAuth.getInstance().currentUser!!.email.toString()
+    }
+
+    private fun bindListeners(){
+
     }
 }
