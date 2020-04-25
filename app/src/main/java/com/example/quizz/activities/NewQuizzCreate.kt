506754d104cd81217e246635aab0e94fe5498e1b
JasonHaque <zahinjason220434@gmail.com>
2020-04-26 02:37:35 +0600

    updated layout

--- app/src/main/java/com/example/quizz/activities/NewQuizzCreate.kt
+++ app/src/main/java/com/example/quizz/activities/NewQuizzCreate.kt
@@ -2,12 +2,39 @@ package com.example.quizz.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import com.example.quizz.R
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_new_quizz_create.*
 
 class NewQuizzCreate : AppCompatActivity() {
+
+    private lateinit var privacyButton : RadioButton
+    var privacy: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_quizz_create)
+        bindListeners()
+    }
+
+    private fun bindListeners(){
+        set_exam.setOnClickListener {
+            val quizName = exam_name.text.toString()
+            val id = radio_privacy.checkedRadioButtonId
+            FirebaseDatabase.getInstance().reference.setValue(privacy)
+
+
+        }
+        radio_privacy.setOnCheckedChangeListener(
+            RadioGroup.OnCheckedChangeListener{group ,checkedId ->
+                val radio:RadioButton = findViewById(checkedId)
+                privacy = radio.text.toString()
+                print(privacy)
+
+            }
+
+        )
     }
 }
